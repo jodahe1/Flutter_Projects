e6e17f851e23200423bc8b3f889a8ff06a181fee
Yodahe Teshome <jodahe2001@gmail.com>
2024-10-14 21:12:32 +0300

    Ui arrangement fot Result Page done

--- bmi/lib/ResultPage.dart
+++ bmi/lib/ResultPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({super.key});
@@ -7,13 +8,56 @@ class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text('BackButton'),
-      )),
-    );
+        appBar: AppBar(
+          title: const Center(child: Text("BMI CALCULATER")),
+        ),
+        body: Column(
+          children: [
+            const Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 18.0),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Your Result',
+                    style: TextStyle(fontSize: 39),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                
+                child: Container(
+                  
+                  width: double.infinity,
+                  color: const Color.fromARGB(255, 8, 34, 61),
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Under Weight',
+                        style: TextStyle(
+                            fontSize: 35,
+                            color: Color.fromARGB(255, 54, 231, 244)),
+                      ),
+                      Text(
+                        '27.8',
+                        style: TextStyle(fontSize: 26),
+                      ),
+                      Text(
+                        'fgdgdgdfgdfg',
+                        style: TextStyle(fontSize: 27),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
